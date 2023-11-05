5ae8429daf02ba0b6f81297003bd562a626e7367
Nikita Kuchur <nikitakuchur@gmail.com>
2023-11-05 01:27:04 +0100

    ignore the test

--- src/test/kotlin/net/englab/ApplicationTest.kt
+++ src/test/kotlin/net/englab/ApplicationTest.kt
@@ -1,18 +1,22 @@
 package net.englab
 
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
 import io.ktor.server.testing.*
 import kotlin.test.*
 import net.englab.plugins.*
 
+@Ignore
 class ApplicationTest {
     @Test
     fun testRoot() = testApplication {
         application {
             configureRouting()
         }
-        //client.get("/api/v1/feedback").apply {
-        //    assertEquals(HttpStatusCode.OK, status)
-        //    assertEquals("Hello World!", bodyAsText())
-        //}
+        client.get("/api/v1/feedback").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
     }
 }
